ffdeb17712d72fd9cb7b9a31037dc7be00576df0
satoshidev999 <ELDAMICOSTANTE12@GMAIL.COM>
2024-09-21 10:57:17 +0300

    Add files via upload

---
+++ Jumia app dev.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        # Add authentication logic here
+        return redirect(url_for('home'))
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
